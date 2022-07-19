f501b37786eb95fea2b49df2c444d06f2e2cfe43
Rogue-striker <jyothikiran52@gmail.com>
2022-07-19 18:22:24 +0530

    added netlify link

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import express from "express";
 import { createServer } from "http";
-import mongoose from "mongoose";
 import { Server } from "socket.io";
-import UserModel from "./Models/UserModel.js";
+
 
 const app = express();
 
@@ -10,42 +9,19 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "*",
+    origin: "https://https://prechat.netlify.app",
   },
-});
-
-// mongoose
-//   .connect("mongodb://localhost:27017/chat", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false,
-//   })
-//   .then(() => {
-//     console.log("connected to mongodb");
-//   })
-//   .catch((err) => {
-//     console.log(err);
-// });
-
-app.get("/", (req, res) => {
-  UserModel.find({ name }, (err, data) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(data);
-    }
-  });
 });
 
 io.on("connection", (socket) => {
   socket.on("register", (data) => {
     socket.join(data.room);
   });
+  socket.on("join", (data)=>{
+    socket.join(data)
+  })
   socket.on("sendMessage", (data) => {
-    console.log(data);
     io.emit("receiveMessage", data);
-//io.to(data.room).emit("receiveMessage", data);
   });
   socket.on("disconnect", () => {
     console.log("user disconnected");
